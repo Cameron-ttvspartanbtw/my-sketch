2df4776f51b4ee7e602f5b90b42b01d4aa6f0a44
Cameron-ttvspartanbtw <124367874+Cameron-ttvspartanbtw@users.noreply.github.com>
2023-02-03 14:25:18 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
-function draw() {
-  background(220);
+function draw(){
+
+
+
+
+}
+
+function mouseDragged(){
+noStroke()
+
+fill(255,10,50)
+circle(mouseX,mouseY,20)
+
 }
